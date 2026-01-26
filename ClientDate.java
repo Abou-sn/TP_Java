71ee96bda3b91eb3153403a2588847af155b7837
Aboubacar Seck <22501608@me.uvsq.fr>
2026-01-26 15:02:25 +0100

    Exo 5

--- ClientDate.java
+++ ClientDate.java
@@ -1,6 +1,8 @@
 public class ClientDate {
     public static void main(String[] args) {
-        Date d = new Date(1, 1, 2026);
+        Date d = new Date(13, 1, 2026);
         System.out.println(d);
+        Date d1 = new Date(2027);
+        System.out.println(d1);
     }
 }
--- Date.java
+++ Date.java
@@ -1,7 +1,7 @@
 public class Date {
-    private int chDay ;
-    private int chMonth ;
-    private int chYear ;
+    private int chDay;
+    private int chMonth;
+    private int chYear;
 
     public Date(int parDay, int parMonth, int parYear) {
         chDay = parDay;
@@ -9,7 +9,18 @@ public class Date {
         chYear = parYear;
     }
 
-    public String toString () {
-        return  chDay + "/" + chMonth + "/" + chYear;
+    public Date(int parYear){
+        chDay = 1;
+        chMonth = 1;
+        chYear = parYear;
+
+    }
+
+    public boolean estValide(Date d){
+        return d.chYear / 4 == 0;
+    }
+
+    public String toString() {
+        return chDay + "/" + chMonth + "/" + chYear;
     }
 }
