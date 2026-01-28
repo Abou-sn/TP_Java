24a4f0a2e1cab604b5e1a950b084bae8d54612ac
Aboubacar Seck <22501608@me.uvsq.fr>
2026-01-28 14:54:17 +0100

    exo6 estValide

--- ClientDate.java
+++ ClientDate.java
@@ -1,8 +1,13 @@
 public class ClientDate {
     public static void main(String[] args) {
+
         Date d = new Date(13, 1, 2026);
         System.out.println(d);
         Date d1 = new Date(2027);
         System.out.println(d1);
+
+        Date dateTest = new Date(31,4,1400);
+
+        System.out.println( dateTest.estValide());
     }
 }
--- Date.java
+++ Date.java
@@ -16,9 +16,51 @@ public class Date {
 
     }
 
-    public boolean estValide(Date d){
-        return d.chYear / 4 == 0;
+    public boolean estBisextile() {
+        return ((chYear%4 == 0 && chYear%100 != 0) || chYear%400 == 0);
     }
+
+    public boolean estValide(){
+
+        if((chYear<1583) || chMonth<0 || chMonth > 12) {
+            return false;
+        }
+
+        if(chDay <0 || chDay>31){
+            return false;
+        }
+
+        if (chMonth == 4 || chMonth == 6 || chMonth == 9 || chMonth == 11) {
+            if (chDay > 30) {
+                System.out.println("Il n'y a que 30 jours pour le mois entré");
+                return false;
+            }
+
+        }
+
+        if (chMonth == 2) {
+
+            if (estBisextile()) {
+                if (chDay > 29) {
+                    System.out.println("Veuillez entrer un jour valide pour le mois de Fevrier");
+                    return false;
+                }
+            }
+            else  {
+                if (chDay > 28) {
+                    System.out.println("Veuillez entrer un jour valide pour le mois de Fevrier");
+                    return false;
+
+
+                }
+            }
+
+        }
+        System.out.println("La date entré est valide ");
+        return true;
+    }
+
+
 
     public String toString() {
         return chDay + "/" + chMonth + "/" + chYear;
