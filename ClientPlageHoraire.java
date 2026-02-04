3d9cdd627563cf354a0b02070b3878c782dfd03d
Aboubacar Seck <22501608@me.uvsq.fr>
2026-02-04 14:33:25 +0100

    Est valide de PlageHoraire

---
+++ ClientPlageHoraire.java
@@ -0,0 +1,14 @@
+public class ClientPlageHoraire {
+    public static void main(String[] args) {
+
+     Horaire debut = new Horaire(20,45);
+     Horaire fin = new Horaire(21,0);
+
+     PlageHoraire ph = new PlageHoraire(debut, fin);
+
+     System.out.println(ph); //Affichage de la plage horaire
+     System.out.println(ph.estValide()); // Affichage de la validité
+
+
+    }
+}
--- PlageHoraire.java
+++ PlageHoraire.java
@@ -1,11 +1,24 @@
 public class PlageHoraire {
-    public final static int duree_min = 30;
-    public Horaire chDebut;
-    public Horaire chFin;
+    private final static int DUREE_MIN = 30;
+    private Horaire chDebut;
+    private Horaire chFin;
 
     public PlageHoraire(Horaire parDebut, Horaire parFin) {
         chDebut = parDebut;
         chFin = parFin;
+    }
+
+    public boolean estValide() {
+        int heureDebut = chDebut.toMinutes();
+        int heureFin = chFin.toMinutes();
+
+        return heureFin-heureDebut >= DUREE_MIN;
+    }
+
+
+
+    public String toString() {
+        return "Debut :"+chDebut + " - " +"Fin :"+ chFin;
     }
 
 }
